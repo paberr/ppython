9acf3f89ad24cdd435de90a478ae719546d7a18c
pb <berrang@cs.uni-saarland.de>
2016-06-15 18:45:55 +0200

    do not save empty commands

--- ppython/history.py
+++ ppython/history.py
@@ -3,9 +3,10 @@ import os
 
 class History:
     def __init__(self):
-        self.history = ['']
-        self.position = 0
-        self.current = 0
+        self._history = ['']  # contains the whole history
+        self._position = 0  # used when navigating through the history
+        # this is the end-index of the history, it might be < len(history)-1 if we appended future commands
+        self._current = 0
 
     def load_from_file(self, filename, end=True):
         """
@@ -24,12 +25,12 @@ class History:
         # if append to the end, remove current and append afterwards
         current_line = ''
         if end:
-            current_line = self.history.pop(self.current)
-        self.history.extend(hist) # append
+            current_line = self._history.pop(self._current)
+        self._history.extend(hist)  # append
         if end:
-            self.history.append(current_line)
-            self.current = len(self.history) - 1
-            self.position = self.current
+            self._history.append(current_line)
+            self._current = len(self._history) - 1
+            self._position = self._current
         return True
 
     def edit(self, line):
@@ -38,38 +39,45 @@ class History:
         :param line: The new text
         :return:
         """
-        self.position = self.current
-        self.history[self.current] = line
+        self._position = self._current
+        self._history[self._current] = line
 
     def commit(self):
         """
         Saves changes to current line and creates new history entry.
         :return:
         """
-        if self.position > self.current:
-            del self.history[self.current]
-            self.current = self.position
+        if self._position > self._current:
+            # drop empty commands from history
+            if len(self._history[self._current].strip()) == 0:
+                del self._history[self._current]
+            # if we execute a future command, set current to that position
+            self._current = self._position
         else:
-            self.current += 1
-            self.position = self.current
-        self.history.insert(self.current, '')
+            # just reset and reuse that entry if empty
+            if len(self._history[self._current].strip()) == 0:
+                self._position = self._current
+                self._history[self._current] = ''
+                return
+            # otherwise, we have to increment current and reset position
+            self._current += 1
+            self._position = self._current
+        self._history.insert(self._current, '')
 
     def move_up(self):
         """
         Moves up in the history entries.
         :return: Entry above
         """
-        if self.position > 0:
-            self.position -= 1
-        return self.history[self.position]
+        if self._position > 0:
+            self._position -= 1
+        return self._history[self._position]
 
     def move_down(self):
         """
         Moves down in the history entries.
         :return: Entry below
         """
-        if self.position < len(self.history) - 1:
-            self.position += 1
-        return self.history[self.position]
-
-
+        if self._position < len(self._history) - 1:
+            self._position += 1
+        return self._history[self._position]
