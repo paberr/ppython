e235dc7cbdd076af01479bfe16a551ec1b399fe5
pb <berrang@cs.uni-saarland.de>
2016-06-15 19:05:44 +0200

    use compilation to find more complicated blocks

--- ppython/interpreter.py
+++ ppython/interpreter.py
@@ -10,6 +10,13 @@ class Interpreter:
         self._environment = dict()
         self.comments = re.compile(r'#.*$', re.MULTILINE)
         self.identifier = re.compile(r'(?:^|\s)((?:[^\d\W]\w*\.)*(?:[^\d\W]\w*))$')
+        # blockopeners are :({[ and """,'''
+        # currently, we find them by explicitly looking for compilation errors
+        # TODO: find better way to do this
+        self.blockopeners = (
+            'EOF while scanning triple-quoted string literal',
+            'unexpected EOF while parsing'
+        )
 
     def inblock(self):
         """
@@ -35,33 +42,50 @@ class Interpreter:
                 # leave if line empty or last empty and only spaces
                 if len(line) == 0 or (self._last_empty and only_spaces):
                     code = '\n'.join(self._aggregate)
+                    compilation = self._try_compile(code)
+                    if compilation is False:
+                        return
                     self._aggregate = []
                     self._inblock = False
                     self._last_empty = False
-                    self._exec(code)
+                    self._exec(compilation)
                 elif only_spaces:
                     # only spaces, so we need two such lines
                     self._last_empty = True
                 else:
                     self._last_empty = False
-            return # if empty line
-        elif stripped_line.endswith(':'):
-            self._inblock = True
+            return  # if empty line
+
+
+        if not self._inblock:
+            compilation = self._try_compile(line)
 
         self._last_empty = False
         if self._inblock:
             self._aggregate.append(line)
         else:
-            self._exec(line)
+            self._exec(compilation)
 
-    def _exec(self, code):
+    def _try_compile(self, line):
+        try:
+            return compile(line, '<input>', 'single')
+        except SyntaxError as e:
+            if e.msg in self.blockopeners:
+                self._inblock = True
+            else:
+                traceback.print_exc()
+        except Exception as e:
+            traceback.print_exc()
+        return False
+
+    def _exec(self, compilation):
         """
         Executes some code and catches exceptions.
         :param code: Code to execute
         :return:
         """
         try:
-            exec(compile(code, '<input>', 'single'), self._environment)
+            exec(compilation, self._environment)
         except Exception as e:
             traceback.print_exc()
 
